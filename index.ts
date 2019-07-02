7a359ad7cb16da471b49d0caa73f97fe7eafe7c1
Hao Yang <tillohao@gmail.com>
2019-07-02 13:45:24 +0800

    <feat> add script update

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useCamera } from './_useCamera';
-export { default as useMicrophone } from './_useMicrophone';
-export { default as useMediaStream } from './_useMediaStream';
--- internals/update.js
+++ internals/update.js
@@ -1,18 +1,24 @@
+const fs = require('fs')
+const path = require('path')
 const shell = require('shelljs')
 const chalk = require('chalk')
 
-process.stdout.write(chalk.yellow('Cleanup started...'));
-
+process.stdout.write(chalk.yellow('Cleanup...'));
 shell.rm('-rf', 'src/components/_*');
 shell.rm('-rf', 'src/configures/_*');
 shell.rm('-rf', 'src/hooks/_*');
-
 process.stdout.write(' ✅\n')
-process.stdout.write(chalk.yellow('Update started...'));
 
+process.stdout.write(chalk.yellow('Update template code...'));
 shell.cp('template/src/components/*', 'src/components/');
 shell.cp('template/src/configures/*', 'src/configures/');
 shell.cp('template/src/hooks/*', 'src/hooks/');
+process.stdout.write(' ✅\n')
 
-process.stdout.write(' ✅')
-
+process.stdout.write(chalk.yellow('Update dependencies...'));
+const currentPkg = require(path.resolve(process.cwd(), 'package.json'));
+const newPkg = require(path.resolve(process.cwd(), 'template/package.json'));
+currentPkg.dependencies = Object.assign({}, currentPkg.dependencies, newPkg.dependencies)
+currentPkg.devDependencies = Object.assign({}, currentPkg.devDependencies, newPkg.devDependencies)
+fs.writeFileSync('package.json', JSON.stringify(currentPkg, null, 2))
+process.stdout.write(' ✅\n')
